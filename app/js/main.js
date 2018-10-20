a3f7a8b8045005a10c218ca6d094bd361ca49d10
Vladislav Mihov <37806520+skilldeliver@users.noreply.github.com>
2018-10-20 17:10:26 +0300

    removing <a> tag in brand name

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,4 @@
 
 var element = document.getElementById("brand-name");
 var date = new Date().getFullYear();
-element.innerHTML = "Алфа Стил ЕООД " + date + " ©";
+element.innerHTML = "Алфа Стил EOOД " + date + " ®"
